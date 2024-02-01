9ed178000464a3512c3eab2b87dd6bf0b657208d
tausheef <tausheefpvt@gmail.com>
2024-02-01 12:52:09 +0530

    props and tailwinds

--- tailwindsprops/src/App.jsx
+++ tailwindsprops/src/App.jsx
@@ -1,14 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './componenets/Card';
 
 function App() {
-
+let myObj = {
+  username: "tauseef",
+  age: 21
+}
 
   return (
     <>
-    <h1 className=''>Hello-my-name </h1>
+    <h1 className='bg-green-500 text-black rounded-xl mb-4'>tailwind-Test </h1>
+     <Card username="chaiandcode"/>
+     <Card username="tauseefedits"/>
+     <Card username="youareawesome"/>
+     <Card username="heythisis"/>
+     
     </>
   )
 }
